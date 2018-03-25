1488cb37c6b5350086d7dd60da1714e78226ca1c
Maxim Balandin <balandus@bk.ru>
2018-03-25 20:08:24 +0500

    Update main.php

--- main.php
+++ main.php
@@ -45,9 +45,8 @@ try {
         echo 'access token: ' . $token
             . '<br />user id: ' . $idvk . '<br /><br />';
     
-           mysqli_query($con,
-        	"UPDATE users SET VK_TOKEN='$token' WHERE LOGIN='$log';");
-	    	"UPDATE users SET VK_ID='$idvk' WHERE LOGIN='$log';");
+           mysqli_query($con,"UPDATE users SET VK_TOKEN='$token' WHERE LOGIN='$log';");
+	   mysqli_query($con,"UPDATE users SET VK_ID='$idvk' WHERE LOGIN='$log';");
         $user_friends = $vk->api('friends.get', array(
             'uid'       => '12345',
             'fields'    => 'uid,first_name,last_name',

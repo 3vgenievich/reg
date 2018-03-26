72ddf53f352f94e0bc9fb5dab0ad454e30501aef
Maxim Balandin <balandus@bk.ru>
2018-03-27 00:24:09 +0500

    Add files via upload

--- posting_script.php
+++ posting_script.php
@@ -8,35 +8,32 @@ $tminus=time()+(17700);
 $perm_time=( date('Y-m-d H:i:s', $t));
 $permtimeminus=( date('Y-m-d H:i:s', $tminus));
 
-echo ($perm_time);
-echo ($permtimeminus);  
-
-
+#echo ($perm_time);
+#echo "$permtimeminus<br>";  
 
 $query ="SELECT * FROM posts WHERE DATETIME BETWEEN '$permtimeminus' AND '$perm_time'";
- 
-$result = mysqli_query($con, $query) or die("Ошибка " . mysqli_error($con)); 
-if($result)
-{
+$result = mysqli_query($con, $query);
+
     $rows = mysqli_num_rows($result); // количество полученных строк
+    echo($rows);
     for ($i = 0 ; $i < $rows ; ++$i)
     {
-        $row = mysqli_fetch_row($result);
-            for ($j = 0 ; $j < 7 ; ++$j) echo "$row[$j]";
-            if($j=6)
-            {
-                $login=$row[0];
-                $text =$row[3];
-                $img=$row[5];
-                $tw=$row[2];
-                $vk=$row[1];
+       while( $row = mysqli_fetch_assoc($result)){
+                $login=$row['LOGIN'];
+                $text =$row['TEXT'];
+                $img=$row['IMG'];
+                $tw=$row['VK'];
+                $vk=$row['TW'];
+                };
                 $VKTOKEN=mysqli_query($con,"SELECT VK_TOKEN FROM users WHERE LOGIN ='$login'");
                 $VKID=mysqli_query($con,"SELECT VK_ID FROM users WHERE LOGIN ='$login'");
                 $TWITTERTOKEN=mysqli_query($con,"SELECT TW_TOKEN FROM users WHERE LOGIN ='$login'");
                 $TWITTER_SECRET=mysqli_query($con,"SELECT TW_VERIFYER FROM users WHERE LOGIN ='$login'");
+                $CONSUMERKEY='fKvwccSUHi7TVX6RvFZtUTNk8';
+                $CONSUMERSECRET='jlH8AK22YuWP5rqmMlaGgqD9Hg7LonEAkHw0pTI1qcIqiIgVrA';
             
                 if ($tw==1){
-                    $connection = new TwitterOAuth(fKvwccSUHi7TVX6RvFZtUTNk8, jlH8AK22YuWP5rqmMlaGgqD9Hg7LonEAkHw0pTI1qcIqiIgVrA, $TWITTERTOKEN, $TWITTER_SECRET);
+                    $connection = new TwitterOAuth($CONSUMERKEY,$CONSUMERSECRET , $TWITTERTOKEN, $TWITTER_SECRET);
                     $content = $connection->get('account/verify_credentials');
                     $connection->post('statuses/update', array('status' => '$text'));
                 }
@@ -57,16 +54,16 @@ if($result)
                         echo 'Error: <b>' . $e->getMessage() . '</b><br />';
                         echo 'in file "' . $e->getFile() . '" on line ' . $e->getLine();
                                             }
-                }
+                    }
 
             }
-        echo "</tr>";
-    }
-    echo "</table>";
+       
+    
+   
      
     // очищаем результат
     mysqli_free_result($result);
-}
+
  
 mysqli_close($con);
 ?>
